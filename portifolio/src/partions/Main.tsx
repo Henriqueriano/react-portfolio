9b8fc30f9c1f025a802e92f69e97750eac633ab1
Daniel Henrique <daniel.neves0021@gmail.com>
2025-08-16 01:20:58 -0300

    feat: some categories

--- portifolio/src/partions/Main.tsx
+++ portifolio/src/partions/Main.tsx
@@ -2,12 +2,20 @@ import "tailwindcss";
 function Main() 
 {
     return (
-        <div className="mx-40 border-[1] rounded-[10]">
-            <p className="text-center italic font-bold font-sans">"Cogito, ergo sum!" ~ René Descartes</p>
+        <div className="mx-40">
+            <div className="mx-2">
+                    <h2 className="text-[#2596be] text-2xl font-bold mb-2">Almejos:</h2>
+                    <p>Como cientista da computação, meu objetivo principal 
+                    é entender novas tecnologias e inovar o mercado por 
+                    meio de aplicações práticas de teorias estudadas e 
+                    desenvolvidas. Desejo coordenar ou atuar o 
+                    desenvolvimento de soluções e garantir o meu 
+                    crescimento pessoal e profissional. </p>
+            </div>
             <div className="columns-2 mx-2 my-2">
                 <div id="colum_one">
                     <div className="my-3">
-                    <h2 className="text-[#2596be] text-2xl font-bold">Graduação:</h2>
+                    <h2 className="text-[#2596be] text-2xl font-bold mb-2">Graduação:</h2>
                     <p>Universidade de Vila Velha - UVV</p>
                     <ul className="ms-5">
                         <li><p>Bacharelado em Cieências da Computação</p></li>
@@ -15,25 +23,45 @@ function Main()
                     </ul>
                     </div>
                     <div className="my-3">
-                    <h2 className="text-[#2596be] text-2xl font-bold">Instruções:</h2>
-                                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus numquam nulla deleniti amet omnis non aliquam animi, architecto consequuntur culpa voluptatum vero totam velit exercitationem suscipit ipsam corporis ipsa inventore?</p>
+                    <h2 className="text-[#2596be] text-2xl font-bold mb-2">Contatos:</h2>
+                    <ul>
+                        <li>
+                            <p>• Email: <a href="mailto:daniel.neves0021@gmail.com">daniel.neves0021@gmail.com</a></p>
+                        </li>
+                        <li>
+                            <p>• Whatsapp: <a href="https://wa.me/5527997740242">+55 (27) 99774-0242</a></p>
+                        </li>
+                        <li>
+                            <p>• Instagram: <a href="https://www.instagram.com/henriquenf__/"> @henriquenf__</a></p>
+                        </li>
+                        <li>
+                            <p>• X: <a href="https://x.com/S0Henrique">@S0Henrique</a></p>
+                        </li>
+                        <li>
+                            <p>• Logradouro: <a href="https://www.google.com.br/maps/place/R.+Manguari+-+Maria+Niobe,+Serra+-+ES,+29176-284/@-20.1302601,-40.3027845,18z/data=!3m1!4b1!4m6!3m5!1s0xb81ffba05de98b:0xabeae7a75856bb3a!8m2!3d-20.130525!4d-40.3011832!16s%2Fg%2F1ttpj3xk?entry=ttu&g_ep=EgoyMDI1MDgxMy4wIKXMDSoASAFQAw%3D%3D"> 29176-284</a></p>
+                        </li>
+                        <li>
+                            <p>• LinkedIn: <a href="https://www.linkedin.com/in/dhnf/"> Daniel Henrique</a></p>
+                        </li>
+                    </ul>
+
                     </div>
                     <div className="my-3">
-                    <h2 className="text-[#2596be] text-2xl font-bold">Instruções:</h2>
+                    <h2 className="text-[#2596be] text-2xl font-bold mb-2">Instruções:</h2>
                                         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus numquam nulla deleniti amet omnis non aliquam animi, architecto consequuntur culpa voluptatum vero totam velit exercitationem suscipit ipsam corporis ipsa inventore?</p>
                     </div>
                 </div>
                 <div id="colum_two">
                     <div className="my-3">
-                    <h2 className="text-[#2596be] text-2xl font-bold">Instruções:</h2>
+                    <h2 className="text-[#2596be] text-2xl font-bold mb-2">Destaques:</h2>
+                                        <p>. . .</p>
+                    </div>
+                    <div className="my-3">
+                    <h2 className="text-[#2596be] text-2xl font-bold mb-2">Instruções:</h2>
                                         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus numquam nulla deleniti amet omnis non aliquam animi, architecto consequuntur culpa voluptatum vero totam velit exercitationem suscipit ipsam corporis ipsa inventore?</p>
                     </div>
                     <div className="my-3">
-                    <h2 className="text-[#2596be] text-2xl font-bold">Instruções:</h2>
-                                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus numquam nulla deleniti amet omnis non aliquam animi, architecto consequuntur culpa voluptatum vero totam velit exercitationem suscipit ipsam corporis ipsa inventore?</p>
-                    </div>
-                    <div className="my-3">
-                    <h2 className="text-[#2596be] text-2xl font-bold">Instruções:</h2>
+                    <h2 className="text-[#2596be] text-2xl font-bold mb-2">Instruções:</h2>
                                         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus numquam nulla deleniti amet omnis non aliquam animi, architecto consequuntur culpa voluptatum vero totam velit exercitationem suscipit ipsam corporis ipsa inventore?</p>
                     </div>
                 </div>
